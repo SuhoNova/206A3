62bb48334eaf9df4fb903905d6c6efef34c557d4
whattupjellycup <gman485@aucklanduni.ac.nz>
2016-09-12 17:37:12 +1200

    made mastered, failed, faulted hidden. created wordlist class

--- src/spellingAid/SpellingAid.java
+++ src/spellingAid/SpellingAid.java
@@ -14,13 +14,13 @@ public class SpellingAid {
 		//create necessary files on startup
 		PrintWriter outputFile;
 		try{
-			outputFile = new PrintWriter(new FileWriter("mastered", true));
+			outputFile = new PrintWriter(new FileWriter(".mastered", true));
 			outputFile.close();
-			outputFile = new PrintWriter(new FileWriter("faulted", true));
+			outputFile = new PrintWriter(new FileWriter(".faulted", true));
 			outputFile.close();
-			outputFile = new PrintWriter(new FileWriter("failed", true));
+			outputFile = new PrintWriter(new FileWriter(".failed", true));
 			outputFile.close();
-			outputFile = new PrintWriter(new FileWriter("stats", true));
+			outputFile = new PrintWriter(new FileWriter(".stats", true));
 			outputFile.close();
 		} catch (IOException e) {
 			e.printStackTrace();
--- src/spellingAid/SpellingAidModel.java
+++ src/spellingAid/SpellingAidModel.java
@@ -10,39 +10,20 @@ import java.util.Random;
 public class SpellingAidModel {
 	private Festival _voice = new Festival("American");
 	private int _quizLevel = 1;
-	private String _quizType;
-
-	private ArrayList<String> _wordList;
+	private String _quizType = "Normal";
+	private WordList _wordList;
+	
 	private final int MAX_ATTEMPTS = 2;
-	private int _quizLength = 3;
+	private int _quizLength = 10;
 	private final static int _DEFAULT_QUIZ_LENGTH =10;
 	private int _nWords;
-
 	private String _attempt;
 	private String _word;
 	private int _nWordsCount=0;
 	private int _nAttempts=0;
 
-	public void setQuizLevel(int level) {
-		_quizLevel = level;
-		System.out.println(_quizLevel);
-	}
-
-	public void setQuizType(String type) {
-		_quizType = type;
-		System.out.println(_quizType);
-	}
-
-	public void setVoiceType(String type) {
-		_voice.changeVoice(type);
-		System.out.println(type);
-	}
-	public int getQuizLevel() {
-		return _quizLevel;
-	}
-
 	public void startQuiz(){
-		createWordList("NZCER-spelling-lists.txt");
+		_wordList = new WordList(_quizType, _quizLevel);
 		if(_wordList.size() < _quizLength){
 			_nWords = _wordList.size();
 		}
@@ -61,11 +42,11 @@ public class SpellingAidModel {
 			System.out.println("Correct");
 			if(_nAttempts == 1){
 				FileManager fm = new FileManager();
-				fm.handleQuizzedWords(_word, "mastered");
+				fm.handleQuizzedWords(_word, ".mastered");
 			}
 			else{
 				FileManager fm = new FileManager();
-				fm.handleQuizzedWords(_word, "faulted");
+				fm.handleQuizzedWords(_word, ".faulted");
 			}
 			if(_nWordsCount>=_nWords){
 			}
@@ -76,11 +57,13 @@ public class SpellingAidModel {
 		else{
 			if(_nAttempts < MAX_ATTEMPTS){
 				_voice.speakIt("Incorrect, try once more..... "+_word+"..... "+_word);
+				System.out.println("Incorrect, try once more..... "+_word+"..... "+_word);
 			}
 			else{
 				FileManager fm = new FileManager();
-				fm.handleQuizzedWords(_word, "failed");
+				fm.handleQuizzedWords(_word, ".failed");
 				_voice.speakIt("Incorrect");
+				System.out.println("Incorrect");
 				if(_nWordsCount>=_nWords){
 				}
 				else{
@@ -90,39 +73,32 @@ public class SpellingAidModel {
 			}
 		}
 	}
-
-	//sets the word being tested
-	private void getWord() {
-		Random r = new Random();
-		int pos = r.nextInt(_wordList.size());
-		_word = _wordList.get(pos);
-		_wordList.remove(pos);
-	}
-
-	//creates the list of possible words that can be used for the quiz
-	private void createWordList(String source){
-		try {
-			_wordList = new ArrayList<String>();
-			BufferedReader br = new BufferedReader(new FileReader(source));
-			String word;
-			while((word = br.readLine())!= null){
-				word = word.trim();
-				if(word.length()>0){
-					_wordList.add(word);
-				}
-			}
-			br.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
 	private void continueQuiz(){
 		_nWordsCount++;
-		getWord();
+		_word = _wordList.getWord();
 		_nAttempts = 0;
 		//_voice.speakIt("Spell word "+_nWordsCount+" of word "+_nWords);
 		_voice.speakIt("Please spell the word: "+_word+"...... "+_word);
+		System.out.println("Please spell the word: "+_word+"...... "+_word);
 	}
-
+	
+	public void setQuizLevel(int level) {
+		_quizLevel = level;
+		System.out.println(_quizLevel);
+	}
+	public void setQuizType(String type) {
+		_quizType = type;
+		System.out.println(_quizType);
+	}
+	public void setVoiceType(String type) {
+		_voice.changeVoice(type);
+		System.out.println(type);
+	}
+	public int getQuizLevel() {
+		return _quizLevel;
+	}
+	public int getWordCount(){
+		return _nWordsCount;
+	}
 
 }
